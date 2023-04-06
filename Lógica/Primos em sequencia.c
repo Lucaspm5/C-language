95850a8f630945d2950e06a48517e8b0a8161c6d
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-04-06 11:45:49 -0300

    Update Primos em sequencia.c

--- Lógica/Primos em sequencia.c
+++ Lógica/Primos em sequencia.c
@@ -4,7 +4,8 @@
 int isPrimo(int x){
     if(x <= 1)
         return 0;
-    
+    if(x == 2)
+        return 1;
     for(int i = 2; i <= sqrt(x);i++){
         if(x % i == 0){
             return 0;
