b467a51ba52fbbb4aec4a4707fc060f0a541f9a2
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-05-30 22:24:46 -0300

    Update 1.c

--- Recursão/Lista 1/1.c
+++ Recursão/Lista 1/1.c
@@ -2,12 +2,11 @@
 
 int fatorial(int n)
 {
-    if (n == 0 || n == 1) return 1;
-
-    else return n * fatorial(n - 1);
+    return (n == 0 || n == 1) ? 1 : n * fatorial (n - 1);
 }
 
-int main(){
+int main()
+{
 
     int fat;
 
