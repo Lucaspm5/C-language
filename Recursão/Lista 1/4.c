aef42d4fcb0f55907e1278eb10776adee7ef36fb
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-05-30 22:43:46 -0300

    Update 4.c

--- Recursão/Lista 1/4.c
+++ Recursão/Lista 1/4.c
@@ -2,9 +2,7 @@
 
 int somar_vetor(int *arr, int N)
 {
-    if (arr == NULL || N == 0) return 0;
-
-    else return  arr[0] + somar_vetor(arr + 1, N - 1);
+    return (arr == NULL || N == 0) ? 0 : arr[0] + somar_vetor(arr + 1, N - 1);
 }
 
 int main()
@@ -15,8 +13,7 @@ int main()
 
     int arr[N];
 
-    for (int i = 0;i < N;i++)
-        scanf("%d", &arr[i]);
+    for (int i = 0;i < N;i++) scanf("%d", &arr[i]);
 
     int result = somar_vetor(arr, N);
 
