c63311483f0e4bef28d0b92c46255714df155202
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-05-30 23:13:59 -0300

    Update 7.c

--- Recursão/Lista 1/7.c
+++ Recursão/Lista 1/7.c
@@ -9,12 +9,7 @@ void trocar(int *a, int *b)
 
 void inverter(int *inicio, int *fim)
 {
-    if (inicio >= fim || inicio == NULL || fim == NULL) return 0;
-
-    else{
-        trocar(inicio, fim);
-        inverter(inicio + 1, fim - 1);
-    }
+    return (inicio >= fim) ? 0 : (trocar(inicio, fim), inverter(inicio + 1, fim - 1));
 }
 int main()
 {
