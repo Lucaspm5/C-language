1ea544f257ddcf93cec5afdc2ec3d242e158c387
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-06-11 16:21:04 -0300

    Update 1.c

--- Recursão/Lista 1/1.c
+++ Recursão/Lista 1/1.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-int fatorial(int n)
-{
-    return (n == 0 || n == 1) ? 1 : n * fatorial (n - 1);
+int fatorial(int n, int* auxiliary, bool* calculado) {
+    if (calculado[n]) return auxiliary[n];
+    if (n == 1) return 1;
+    else {
+        return auxiliary[n] = n * fatorial(n - 1, auxiliary, calculado);
+    }
+    calculado[n] = true;
+    return auxiliary[n];
 }
 
-int main()
-{
+int main() {
+    int n = 5;
+    int auxiliary[n+1];
+    bool calculado[n+1];
+    memset(calculado, false, sizeof(calculado));
 
-    int fat;
-
-    scanf("%d", &fat);
-
-    printf("%d! = %d" , fat, fatorial(fat));
-
-    return 0;
+    printf("%d\n", fatorial(n, auxiliary, calculado));
 }
